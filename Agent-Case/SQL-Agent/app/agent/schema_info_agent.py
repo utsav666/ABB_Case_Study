a8bdfe95e9f2efb1638df4868a0a16b4d2b9188d
Utsav <utsavsinha93@gmail.com>
2025-11-14 07:49:20 +0530

    pushing the 1st router

--- Agent-Case/SQL-Agent/app/agent/schema_info_agent.py
+++ Agent-Case/SQL-Agent/app/agent/schema_info_agent.py
@@ -16,12 +16,19 @@ class SchemaInfoAgent:
             user_question = self.user_question,
             context = self.context
         )
+        print(prompt)
         response = openai.chat.completions.create(
         model="gpt-4",
         messages=[
-            {"role": "system", "content": "You are a world-class SQL generator."},
+            {"role": "system" ,"content": "You are a Schema expert."},
             {"role": "user", "content": prompt}
         ],
         temperature=0
     )
         return response.choices[0].message.content.strip()
+
+if __name__ == "__main__":
+    schema_info_agent = SchemaInfoAgent("what are the columns of movie table ? ",
+                                        "This only need schema detail")
+    sample_Res = schema_info_agent.run()
+    print(sample_Res)
--- Agent-Case/SQL-Agent/app/orchestrator/orch_exec.py
+++ Agent-Case/SQL-Agent/app/orchestrator/orch_exec.py
@@ -3,6 +3,7 @@ import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from agent.sql_generation_agent import SQLGenerationAgent
 from agent.table_identification_agent import TableIdentiferAgent
+from agent.schema_info_agent import SchemaInfoAgent
 from router.route import Router
 from utils.sql_validator import validator
 from utils.sql_executor import SqlExec
@@ -16,24 +17,33 @@ class Orchestrator:
     def executor(self):
         print("user question .... "+self.user_question)
         print("\n.....Running the Table Identifier Agent ......")
-        ta_agent_result = self.TA.run()
-        print("\n....Table identifier result... "+str(ta_agent_result) )
-        print("\nRunning SQL Generation Agent")
-        sql_gen_agent = SQLGenerationAgent(self.user_question , ta_agent_result['relevant_tables'])
-        sql_gen_res = sql_gen_agent.run()
-        print("\n....SQL generation Result... "+sql_gen_res)
-        sql_val = validator(sql_gen_res)
-        if sql_val is True:
-            print("\n.........executing query......")
-            sql_exec = SqlExec().sql_executor(sql_gen_res)
-            #print(sql_exec.shape)
-            return sql_exec
+        decision_maker = self.router.route_question(self.user_question)
+        print(decision_maker,"decision_maker")
+        if decision_maker["route"]== "sql_query":
+            ta_agent_result = self.TA.run()
+            print("\n....Table identifier result... "+str(ta_agent_result) )
+            print("\nRunning SQL Generation Agent")
+            sql_gen_agent = SQLGenerationAgent(self.user_question , ta_agent_result['relevant_tables'])
+            sql_gen_res = sql_gen_agent.run()
+            print("\n....SQL generation Result... "+sql_gen_res)
+            sql_val = validator(sql_gen_res)
+            if sql_val is True:
+                print("\n.........executing query......")
+                sql_exec = SqlExec().sql_executor(sql_gen_res)
+                #print(sql_exec.shape)
+                return sql_exec
+            else:
+                print("\nFailed to generate sql")
         else:
-            print("\nFailed to generate sql")
+            schema_info_agent = SchemaInfoAgent(self.user_question,
+                                                decision_maker["reasoning"])
+            res = schema_info_agent.run()
+            print(res)
+            return res
         
 #### Unite Test 3 #########    
 # if __name__ == "__main__":
-#     user_query = "top 5 movie name based on rating"
+#     user_query = "what are the columns of movie table ?"
 #     print("..Orchestration Started.....")
 #     orch_run = Orchestrator(user_query)
 #     orch_run.executor()
